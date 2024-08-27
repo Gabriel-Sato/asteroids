0f964d7e1ab086486a76ef13a752d57ef049972a
Gabriel-Sato <gusato.1@gmail.com>
2024-08-26 22:25:48 -0300

    changed main.py, added frame rate

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     print("Starting asteroids!")
     print(f'Screen width: {SCREEN_WIDTH}')
@@ -13,8 +15,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill((0,0,0))
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
