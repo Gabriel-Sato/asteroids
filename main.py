24537d9c88c2d64f0fe4f429c728aa7790516ee5
Gabriel-Sato <gusato.1@gmail.com>
2024-08-28 17:09:40 -0300

    finished drawing player obj

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y, PLAYER_RADIUS)
+
     print("Starting asteroids!")
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
@@ -17,8 +22,8 @@ def main():
                 return
         
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
-
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
